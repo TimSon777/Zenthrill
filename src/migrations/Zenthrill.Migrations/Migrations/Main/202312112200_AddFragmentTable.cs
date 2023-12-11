d1eb3fc5c86e40bd794d8e0d7852dcbcf04160ad
TimSon777 <dusalimov.tima@yandex.ru>
2023-12-11 23:20:07 +0300

    feature: init main project, migrator, main db

---
+++ src/migrations/Zenthrill.Migrations/Migrations/Main/202312112200_AddFragmentTable.cs
@@ -0,0 +1,16 @@
+﻿namespace Zenthrill.Migrations.Migrations.Main;
+
+[TimestampedMigration(2023, 12, 11, 22, 00)]
+public sealed class AddFragmentTable : ForwardOnlyMigration
+{
+    public override void Up()
+    {
+        Create
+            .Table("Fragment")
+            .WithColumn("Id")
+                .AsGuid()
+                .PrimaryKey()
+            .WithColumn("Body")
+                .AsString();
+    }
+}
