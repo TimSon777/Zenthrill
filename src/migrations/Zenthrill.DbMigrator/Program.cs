4e81e76ce93ac773b7427342a2b98f9a995416cc
TimSon777 <90468548+TimSon777@users.noreply.github.com>
2023-12-11 23:21:20 +0300

    feature: init main project, migrator, main db (#1)

---
+++ src/migrations/Zenthrill.DbMigrator/Program.cs
@@ -0,0 +1,33 @@
+﻿using FluentMigrator.Runner;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Zenthrill.Migrations.Migrations.Main;
+
+var currentDirectory = Directory.GetCurrentDirectory();
+
+var environmentName = Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT")
+    ?? throw new InvalidOperationException("\"DOTNET_ENVIRONMENT\" doesn't set");
+
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(currentDirectory)
+    .AddJsonFile("appsettings.json", true)
+    .AddJsonFile($"appsettings.{environmentName}.json", true)
+    .AddEnvironmentVariables()
+    .Build();
+
+var connectionString = configuration["DATABASE_CONNECTION_STRING"]
+    ?? throw new InvalidOperationException("DATABASE_CONNECTION_STRING doesn't set");
+
+Console.WriteLine(connectionString);
+var serviceProvider = new ServiceCollection()
+    .AddFluentMigratorCore()
+    .ConfigureRunner(rb => rb
+        .AddPostgres()
+        .WithGlobalConnectionString(connectionString)
+        .ScanIn(typeof(AddFragmentTable).Assembly).For.Migrations())
+    .AddLogging(lb => lb.AddFluentMigratorConsole())
+    .BuildServiceProvider();
+
+var migrationRunner = serviceProvider.GetRequiredService<IMigrationRunner>();
+
+migrationRunner.MigrateUp();
